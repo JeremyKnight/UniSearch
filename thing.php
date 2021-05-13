8344e1acdc41f5cedf41e19f16385f4ba011b63b
Jeremy C Knight <jknight22@my.whitworth.edu>
2021-05-12 20:53:49 -0700

    test1

--- thing.php
+++ thing.php
@@ -2,16 +2,16 @@
 <body>
 <?php
 //include('header.php');
-    $SQL = $_REQUEST["stuff"];
+     $SQL = $_REQUEST["stuff"];
     
     echo "Sql: '".$SQL."'";
 
-    $conn = mssql_connect("UniSearchPeople") or die("cannot connect :");
-    echo "connected";
+    // $conn = mssql_connect("UniSearchPeople") or die("cannot connect :");
+    // echo "connected";
     
-    $conn = mssql_connect("cs1") or die("cannot connect :".mssql_error());
-    echo "whats up";
-    //Choose the database to use
-    $db = mssql_select_db("library") or die("cannot open library: ".mssql_error());
+    // $conn = mssql_connect("cs1") or die("cannot connect :".mssql_error());
+    // echo "whats up";
+    // //Choose the database to use
+    // $db = mssql_select_db("library") or die("cannot open library: ".mssql_error());
     />
 </body><html>
