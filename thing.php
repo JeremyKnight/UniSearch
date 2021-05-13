85cdc1437089ff4e8e2111c137bca39acdb3cfae
Jeremy C Knight <jknight22@my.whitworth.edu>
2021-05-12 20:42:13 -0700

    testing php

--- thing.php
+++ thing.php
@@ -7,9 +7,10 @@
     echo "Sql: '".$SQL."'";
 
     $conn = mssql_connect("UniSearchPeople") or die("cannot connect :");
-
-    $conn = mssql_connect("cs1") or die("cannot connect :".mssql_error());
+    echo "connected";
     
+    $conn = mssql_connect("cs1") or die("cannot connect :".mssql_error());
+    echo "whats up";
     //Choose the database to use
     $db = mssql_select_db("library") or die("cannot open library: ".mssql_error());
     />
