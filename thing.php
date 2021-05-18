0bfe1d516173d59fd848898ff1948ca3fbfef200
jeremyknight <jeremyknightjck99@gmail.com>
2021-05-17 17:01:31 -0700

    add echo\ about connection

--- thing.php
+++ thing.php
@@ -4,11 +4,11 @@
 //include('header.php');
      $SQL = $_REQUEST["stuff"];
     
-    echo "Sql: '".$SQL."'";
+    //echo "Sql: '".$SQL."'";
 
      $conn = mssql_connect("UniSearchPeople") or die("cannot connect :");
      //echo "connected";
-    
+    echo $conn;
     // //Choose the database to use
      $db = mssql_select_db("library") or die("cannot open library: ".mssql_error());
     // echo "whats up";
