17eda712f519b408249a3754075ff1812fd3f899
jeremyknight <jeremyknightjck99@gmail.com>
2021-05-18 19:56:33 -0700

    added more with php conditionals

--- thing.php
+++ thing.php
@@ -11,15 +11,26 @@
      $serverName = "tcp:unisearchdb.database.windows.net,1433";
      $conn = sqlsrv_connect($serverName, $connectionInfo);
      
+     //this didn't print out anything...
+     echo "got here";
      if($SQL=="all") {
           echo "all from conditional";
+          $query = "SELECT * FROM DBO.CUSTOMERS";
+          echo $query;
      } else if($SQL=="two") {
           echo "two from conditional";
+          $query = "";
+          echo $query;
      } else if($sql=="one") {
           echo "one from conditional";
+          $query = "SELECT * FROM BDO.CUSTOMERS WHERE NAME IN ('Orlando','Keith')";
+          echo $query;
      } else {
           echo "something is wrong";
+          $query = "SELECT * FROM BDO.CUSTOMERS WHERE NAME IN ('Janet')";
+          echo $query;
      }
+
      //for all: SELECT * FROM DBO.CUSTOMERS
      //FOR 2: 
      //$conn = mssql_connect("UniSearchPeople") or die("cannot connect :");
