61e9430d75b0c9183d2d5948a94748810f08a355
jeremyknight <jeremyknightjck99@gmail.com>
2021-05-19 12:11:07 -0700

    changed database connection string name

--- thing.php
+++ thing.php
@@ -7,7 +7,7 @@
     //echo "Sql: '".$SQL."'";
 
 
-    $connectionInfo = array("UID" => "jkPerson", "pwd" => "serverPassword!", "Database" => "People", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $connectionInfo = array("UID" => "jkPerson", "pwd" => "serverPassword!", "Database" => "UnisearchPeople", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
      $serverName = "tcp:unisearchdb.database.windows.net,1433";
      $conn = sqlsrv_connect($serverName, $connectionInfo)
           or die("failed to connect");
